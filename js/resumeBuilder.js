f55631cd2363b7c76ee7968dbafd44a373d71167
Joshua Nutt <joshuadnutt@gmail.com>
2014-12-18 12:13:31 -0600

    created displayWork() function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -81,21 +81,22 @@ while (bio.skills.length > i) {
 }
 
 
+function displayWork() {	
 
-for (job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
 
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
-	$(".work-entry:last").append(formattedEmployerTitle);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
 
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-	$(".work-entry:last").append(formattedDates);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
 
-	var formattedDescription = HTMLworkDescription.replace("%data", work.jobs[job].description);
-	$(".work-entry:last").append(formattedDescription);
-
+		var formattedDescription = HTMLworkDescription.replace("%data", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+	}
 }
 
-
+displayWork();
